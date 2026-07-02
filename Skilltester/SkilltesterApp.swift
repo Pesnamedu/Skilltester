3472440a1e101f9157a50b213a76cef0833802b7
Lukas Sukdol <lukas.sukdol.jr@gmail.com>
2026-07-02 12:32:26 +0200

    Initial Commit

---
+++ Skilltester/SkilltesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkilltesterApp.swift
+//  Skilltester
+//
+//  Created by Luky on 02.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct SkilltesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
